01f955c5e878be8ae9b661ff99d27f8266a45b3a
nischay maridevegowda <nischaym@gmail.com>
2017-02-09 21:31:04 -0500

    adding draw2D

---
+++ public/js/vendor/draw2D/examples/boilerplate/angular/src/controllers/EditorController.js
@@ -0,0 +1,56 @@
+
+app.controller('EditorController',[ '$scope', "$modal", function($scope,  $modal) {
+	
+
+    $scope.editor = {
+    		// ng-click Callbacks
+            //
+            // Open the FileOpenDialog and let the user select a new file for open
+    		//
+    		fileOpen: function(){
+    		    var modalInstance = $modal.open({
+    		      templateUrl:'src/controllers/FileOpenController.html',
+    		      controller: 'FileOpenController'
+    		    });
+    		    
+    		    modalInstance.result.then(
+    		        // [OK]
+    		    	function (content) {
+    		    	    $scope.editor.load(content);
+	    		    }, 
+	    		    // [Cancel]
+	    		    function () {
+	    		        
+	    		    }
+	    	   );
+    		},
+    		//------------------------------------------------------------------------
+    		
+    		
+    		// Configuration of the editor
+    		//
+    		// 
+            canvas : {
+                // callback if a DOM node from the palette is dropped inside the canvas
+                //
+                onDrop: function(droppedDomNode, x, y, shiftKey, ctrlKey){
+                    var type = $(droppedDomNode).data("shape");
+                    var figure = eval("new "+type+"();");
+                    // create a command for the undo/redo support
+                    var command = new draw2d.command.CommandAdd(this, figure, x, y);
+                    this.getCommandStack().execute(command);
+                }
+            },
+ 
+            // provide all figures to show in the left hand palette
+            // Used by the directrives/canvas.js
+            palette: {
+                    figures: [
+                        {class:"draw2d.shape.basic.Circle", name:"Circle", "cssClass": "draw2d_shape_basic_Circle", attr: {cssClass: "draw2d_shape_basic_Circle"}},
+                        {class:"draw2d.shape.basic.Rectangle", name:"Rectangle", "cssClass": "draw2d_shape_basic_Circle"},
+                        {class:"draw2d.shape.node.Start", name:"Start", "cssClass": "draw2d_shape_basic_Circle"},
+                        {class:"draw2d.shape.node.End"  , name:"End", "cssClass": "draw2d_shape_basic_Circle"},
+                    ]
+            }
+    };
+}]);
