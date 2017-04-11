438fcfe386d7b15dc69812ee9794c704153299e0
Anupma Kushwaha <anupma.kw@gmail.com>
2017-04-11 17:26:38 -0400

    Navigation between website flow and pages done

--- public/ide/views/page/page.controller.js
+++ public/ide/views/page/page.controller.js
@@ -6,7 +6,12 @@
         .module("genXapp")
         .controller("PageController", PageController);
 
-    function PageController() {
-
+    function PageController($routeParams) {
+        //user/:uid/website/:wid/page
+        var vm = this;
+        var uid = ($routeParams.uid);
+        var wid = ($routeParams.wid);
+        vm.uid = uid;
+        vm.wid = wid;
     }
 })();
